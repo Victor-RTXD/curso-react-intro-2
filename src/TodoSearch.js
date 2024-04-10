d0d5c308f169eada1ca25b69afb92dc6003ca891
Victor-RTXD <106888882+Victor-RTXD@users.noreply.github.com>
2024-04-10 23:51:30 +0000

    completada clase 7

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,13 +1,21 @@
+import React from "react";
 import "./TodoSearch.css"
 
 function TodoSearch() {
-    return (
-      <input className="TodoSearch" type="text" placeholder="Cortar cebolla" onChange={(event) => {
-        console.log("este es todosearch");
-        console.log(event)
-        console.log(event.target.value)
-      }} />
-    );
+  // Este es el estado
+  const [searchValue, setSearchValue] = React.useState("");
+
+  console.log("los usuarios buscan todos de: " + searchValue);
+
+  return (
+    <input className="TodoSearch" type="text" placeholder="Cortar cebolla" 
+    value={searchValue}
+    onChange={(event) => {
+      console.log(event);
+      console.log(event.target.value);
+      setSearchValue(event.target.value);
+    }} />
+  );
 }
 
 export { TodoSearch };
