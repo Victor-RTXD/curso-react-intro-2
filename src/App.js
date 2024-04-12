6432a739f6e2a2cc3719e33af29c1d7b8e46f22c
Victor-RTXD <106888882+Victor-RTXD@users.noreply.github.com>
2024-04-12 15:37:24 +0000

    completando clase 8

--- src/App.js
+++ src/App.js
@@ -3,19 +3,34 @@ import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 import { CreateTodoButton } from "./CreateTodoButton";
+import React from "react";
 
 const defaultTodos = [
   {text: "Cortar cebolla", completed: true},
   {text: "tomar curso de react", completed: true},
   {text: "llorar con la llorona", completed: false},
   {text: "renderizando arrays", completed: false},
+  {text: "memeando con chascarrillos", completed: true},
 ]
 
 function App() {
+  // Este es el estado de TodoSearch
+  const [searchValue, setSearchValue] = React.useState("")
+  console.log("los usuarios buscan todos de: " + searchValue);
+
+  // Este es el estado de TodoItem
+  const [todos, setTodos] = React.useState(defaultTodos);
+  console.log("nuevo todo: " + todos);
+
+  const completedTodos = todos.filter(
+    (todo) => todo.completed
+  ).length
+  const totalTodos = todos.length;
+
   return (
     <>
-      <TodoCounter completed={16} total={25}/>
-      <TodoSearch />
+      <TodoCounter completed={completedTodos} total={totalTodos}/>
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
 
       <TodoList>
         {/* aqui vamos a renderizar un array defaultTodos.map() a partir de un array defaultTodos */}
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,12 +1,7 @@
 import React from "react";
 import "./TodoSearch.css"
 
-function TodoSearch() {
-  // Este es el estado
-  const [searchValue, setSearchValue] = React.useState("");
-
-  console.log("los usuarios buscan todos de: " + searchValue);
-
+function TodoSearch({searchValue, setSearchValue}) {
   return (
     <input className="TodoSearch" type="text" placeholder="Cortar cebolla" 
     value={searchValue}
