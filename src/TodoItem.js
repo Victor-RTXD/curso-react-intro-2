fb933e5b8b74e0e87657ab268065e046ac8e75f5
Victor-RTXD <106888882+Victor-RTXD@users.noreply.github.com>
2024-04-20 23:59:14 +0000

    acabando clase 12

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,11 +5,9 @@ import { DeleteIcon } from "./DeleteIcon";
 function TodoItem({text, completed, onComplete, onDelete}) {
     return (
       <li className="TodoItem">
-        {/* <span className={`Icon Icon-check ${completed && "Icon-check--active"}`} onClick={onComplete}>V</span> */}
-        <CompleteIcon />
+        <CompleteIcon completed={completed} onComplete={onComplete}/>
         <p className={`TodoItem-p ${completed && "TodoItem-p--complete"}`}>{text}</p>
-        <DeleteIcon />
-        {/* <span className="Icon Icon-delete" onClick={onDelete}>X</span> */}
+        <DeleteIcon onDelete={onDelete}/>
       </li>
     );
 }
