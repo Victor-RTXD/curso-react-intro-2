692aef8ae6720ee9ec340121361279074aa6b857
Victor-RTXD <106888882+Victor-RTXD@users.noreply.github.com>
2024-05-01 00:01:53 +0000

    acabando clase 21

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -3,6 +3,9 @@ import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
 import { TodoItem } from '../TodoItem';
 import { CreateTodoButton } from "../CreateTodoButton";
+import { TodosLoading } from "../TodosLoading";
+import { TodosError } from "../TodosError";
+import { EmptyTodos } from "../EmptyTodos";
 
 function AppUI({
     loading, 
@@ -23,9 +26,15 @@ function AppUI({
             <TodoList>
             {/* aqui vamos a renderizar un array defaultTodos.map() a partir de un array defaultTodos */}
 
-            {loading && <p>Estamos cargando...</p>} 
-            {error && <p>Desesperate, hubo un error</p>} 
-            {(!loading && searchedTodos.length === 0 ) && <p>crea tu primer todo</p>}
+            {loading && 
+                <>
+                    <TodosLoading />
+                    <TodosLoading />
+                    <TodosLoading />
+                </>
+            } 
+            {error && <TodosError />} 
+            {(!loading && searchedTodos.length === 0 ) && <EmptyTodos />}
 
             {searchedTodos.map(todo => (
                 // Cada componente dentro de un array debe tener una key unica
