2f9f703d4d1417607d853af0d4f0f4769b297475
Victor-RTXD <106888882+Victor-RTXD@users.noreply.github.com>
2024-05-05 19:12:45 +0000

    clase 25

--- src/CreateTodoButton/index.js
+++ src/CreateTodoButton/index.js
@@ -1,10 +1,15 @@
 import "./CreateTodoButton.css";
+import React from "react";
+import { TodoContext } from "../TodoContext";
+
 function CreateTodoButton() {
+    const { openModal, setOpenModal } = React.useContext(TodoContext);
+
+    const toggleModal = () => setOpenModal(!openModal);
+
     return (
       <button className="CreateTodoButton" onClick={
-        (event) => {
-          alert("putos todos");
-        }
+        (event) => { toggleModal() }
       }>+</button>
     );
 }
