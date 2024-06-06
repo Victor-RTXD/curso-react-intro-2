169546794aea937cf72cd7275216e25484159361
Victor-RTXD <106888882+Victor-RTXD@users.noreply.github.com>
2024-06-06 22:57:41 +0000

    acabando clase 30 sabiendo hacer downgrades de react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+// React 18 index.js
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+
+// react 17 index.js
+import React from "react";
+import {render} from "react-dom";
+import "./index.css";
+import App from "./App";
+
+const root = document.getElementById("root");
+render(<App />, root);
